fa3bfaea3c49616e38ef4fe75582df95f1187202
lee2752 <69233948+leedh2752@users.noreply.github.com>
2022-12-13 00:29:24 +0900

    그리톡 count, post update, image name 수정

--- middleWare/multer.js
+++ middleWare/multer.js
@@ -16,7 +16,7 @@ const uploadPost = multer({
         bucket: awsConfig.bucketName+"/generatedImages",
         acl: 'public-read-write',
         key: function(req, file, cb) {
-            cb(null, Math.floor(Math.random() * 1000).toString() + Date.now() + '.' + file.originalname.split('.').pop());
+            cb(null, Math.floor(Math.random() * 1000).toString() + Date.now() + '.png');
         }
     }),
     fileFilter: function(req, file, cb) {
